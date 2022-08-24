7816ce72e655c1b0c7919addaf95f5050f57afc6
wcmsues <wcmsues@163.com>
2022-08-24 13:11:33 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+# 确保脚本抛出遇到的错误
+set -e
+# 生成静态文件
+hugo --theme=LoveIt --baseUrl="https://wcmsues.github.io"
+
+# 项目整体上传到hugo-blog仓库
+# 基本操作
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push git@github.com:wcmsues/hugo-blog.git master
+
+# 强制发布
+# git push -f git@github.com:Milo980412/Milo980412.github.io.git master
+git@github.com:wcmsues/hugo-blog.git
+
+# # public文件夹上传到wcmsues.github.io仓库
+# # 进入生成的文件夹
+# cd public
+
+# # 基本操作
+# git init
+# git add -A
+# git commit -m 'deploy'
+
+# #只在第一次部署时运行，第一次运行完注释掉
+# git remote add origin git@github.com:wcmsues/wcmsues.github.io.git
+
+# # 如果发布到 https://<USERNAME>.github.io
+# git push git@github.com:wcmsues/hugo-blog.git master
